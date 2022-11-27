f90cf592c5ef4f0c68f9de82b1de8a0abe1afcb4
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-27 16:18:53 -0500

    added user routes

---
+++ routes/userRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const User = require('../models/User');
+
+// signup 
+
+router.post('/signup', async(req, res) => {
+    const {name, email, password} = req.body;
+
+    try {
+        const user = await User.create({name, email, password});
+        res.json(user);
+    } catch (e) {
+        if(e.code === 11000) return res.status(400).send('Email already exists')
+    }
+})
+
+// login
+
+router.post('/login', async(req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user = await User.findByCredentials(email, password);
+        res.json(user);
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+})
+
+// get users;
+
+router.get('/', async(req, res) => {
+    try {
+        const users = await User.find({isAdmin: false}).populate('orders');
+        res.json(users)
+    } catch (e) {
+        res.status(400).send(e.message)
+    }
+})
+module.exports = router;
