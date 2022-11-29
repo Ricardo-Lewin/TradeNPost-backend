f8eec5fb5b0410a545b3a96b45b5d843cb82c06c
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-11-28 20:52:05 -0500

    connected user routes

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ const io = new Server(server, {
     methods: '*'
 })
 
+
+const User = require('./models/User');
+const userRoutes = require('./routes/userRoutes');
+
+
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
-
+app.use('/users', userRoutes);
 
 server.listen(8080, () => {
     console.log('server running at port', 8080)
