ddfcd81ac9feff7c6609c3a21936e6c07a525c45
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-01 11:53:25 -0500

    added Product DB Schema

---
+++ models/Product.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const ProductSchema = mongoose.Schema({
+    name: {
+        type: String,
+        requred: [true, "can't be blank"]
+    },
+    description: {
+        type: String,
+        required: [true, "can't be blank"]
+    },
+    price: {
+        type: String, 
+        required: [true, "can't be blank"]
+    },
+    category: {
+        type: String, 
+        required: [true, "can't be blank"]
+    },
+    pictures: {
+        type: Array, 
+        required: true
+    }
+
+}, {minimize: false});
