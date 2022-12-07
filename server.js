fd092c7c3d55f211f0ce13c93741f75211595d39
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-07 18:32:24 -0500

    added image delete route

--- server.js
+++ server.js
@@ -14,12 +14,14 @@ const io = new Server(server, {
 const User = require('./models/User');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
+const imageRoutes = require('./routes/imageRoutes');
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use('/users', userRoutes);
-app.use('products', productRoutes);
+app.use('/products', productRoutes);
+app.use('/images', imageRoutes);
 
 server.listen(8080, () => {
     console.log('server running at port', 8080)
