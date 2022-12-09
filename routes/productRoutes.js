971585352401ec33f53b19901c137334081bca7e
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-08 19:18:32 -0500

    added cart function end points

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -89,4 +89,79 @@ router.get('/category/:category', async(req,res)=> {
       }
 });
 
+// cart routes
+
+router.post('/add-to-cart', async(req, res)=> {
+  const {userId, productId, price} = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    const userCart = user.cart;
+    if(user.cart[productId]){
+      userCart[productId] += 1;
+    } else {
+      userCart[productId] = 1;
+    }
+    userCart.count += 1;
+    userCart.total = Number(userCart.total) + Number(price);
+    user.cart = userCart;
+    user.markModified('cart');
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+})
+
+router.post('/increase-cart', async(req, res)=> {
+  const {userId, productId, price} = req.body;
+  try {
+    const user = await User.findById(userId);
+    const userCart = user.cart;
+    userCart.total += Number(price);
+    userCart.count += 1;
+    userCart[productId] += 1;
+    user.cart = userCart;
+    user.markModified('cart');
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+router.post('/decrease-cart', async(req, res)=> {
+  const {userId, productId, price} = req.body;
+  try {
+    const user = await User.findById(userId);
+    const userCart = user.cart;
+    userCart.total -= Number(price);
+    userCart.count -= 1;
+    userCart[productId] -= 1;
+    user.cart = userCart;
+    user.markModified('cart');
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+})
+
+router.post('/remove-from-cart', async(req, res)=> {
+  const {userId, productId, price} = req.body;
+  try {
+    const user = await User.findById(userId);
+    const userCart = user.cart;
+    userCart.total -= Number(userCart[productId]) * Number(price);
+    userCart.count -= userCart[productId];
+    delete userCart[productId];
+    user.cart = userCart;
+    user.markModified('cart');
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+})
+
 module.exports = router;
