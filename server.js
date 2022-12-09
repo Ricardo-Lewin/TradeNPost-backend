7325efa0ca90114336c137cd2f90a1281964b160
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-09 13:33:38 -0500

    added stripe requirement

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors')
 const app = express();
 const http = require('http');
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_SECRET);
 require('./connection')
 const server = http.createServer(app);
 const {Server} = require('socket.io');
@@ -22,6 +24,23 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/images', imageRoutes);
+
+
+app.post('/create-payment', async(req, res) => {
+    const {amount} = req.body;
+    console.log(amount);
+    try {
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      });
+      res.status(200).json(paymentIntent)
+    } catch (e) {
+      console.log(e.message);
+      res.status(400).json(e.message);
+     }
+})
 
 server.listen(8080, () => {
     console.log('server running at port', 8080)
