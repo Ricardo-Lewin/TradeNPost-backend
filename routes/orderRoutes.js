b3811a77215d06f47d1d0c589043d06914f3f9ee
Ricky <ricardo.a.lewin@gmail.comgit config user.email ricardo.a.lewin@gmail.com>
2022-12-10 19:41:19 -0500

    added orders mongo object and route

---
+++ routes/orderRoutes.js
@@ -0,0 +1,41 @@
+const router = require('express').Router();
+const Order = require('../models/Order');
+const User = require('../models/User');
+
+
+//creating an order
+
+router.post('/', async(req, res)=> {
+  const {userId, cart, country, address} = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    const order = await Order.create({owner: user._id, products: cart, country, address});
+    order.count = cart.count;
+    order.total = cart.total;
+    await order.save();
+    user.cart =  {total: 0, count: 0};
+    user.orders.push(order);
+    user.markModified('orders');
+    await user.save();
+    res.status(200).json(user)
+
+  } catch (e) {
+    res.status(400).json(e.message)
+  }
+})
+
+
+// getting all orders;
+router.get('/', async(req, res)=> {
+  try {
+    const orders = await Order.find().populate('owner', ['email', 'name']);
+    res.status(200).json(orders);
+  } catch (e) {
+    res.status(400).json(e.message)
+  }
+})
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const User = require('./models/User');
 const userRoutes = require('./routes/userRoutes');
 const productRoutes = require('./routes/productRoutes');
 const imageRoutes = require('./routes/imageRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 app.use(cors());
 app.use(express.urlencoded({extended: true}));
@@ -24,6 +25,7 @@ app.use(express.json());
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/images', imageRoutes);
+app.use('/orders', orderRoutes);
 
 
 app.post('/create-payment', async(req, res) => {
